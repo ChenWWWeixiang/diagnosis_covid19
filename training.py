7ad26399abcd11cc690613adc95d27a3accf1514
cwx <chenwx_thu@foxmail.com>
2020-09-23 16:51:37 +0800

    new vision

--- training.py
+++ training.py
@@ -1,25 +1,30 @@
 from torch.autograd import Variable
 import warnings
+
 warnings.filterwarnings("ignore")
 import torch
 import torch.optim as optim
 from datetime import datetime, timedelta
-from data.dataset import NCPDataset,NCP2DDataset,NCPJPGDataset,NCPJPGDataset_new,NCPJPGtestDataset_new
+from data.dataset import NCPDataset, NCP2DDataset, NCPJPGDataset, NCPJPGDataset_new, NCPJPGtestDataset_new
 from torch.utils.data import DataLoader
 from tensorboardX import SummaryWriter
 import torch.nn as nn
-import os,cv2,shutil
+import os, cv2, shutil
 import pdb
 import math
-USE_25D=False
+
+USE_25D = False
+
+
 class NLLSequenceLoss(torch.nn.Module):
     """
     Custom loss function.
     Returns a loss that is the sum of all losses at each time step.
     """
-    def __init__(self,w=[0.55, 0.45]):
+
+    def __init__(self, w=[0.55, 0.45]):
         super(NLLSequenceLoss, self).__init__()
-        self.criterion = torch.nn.NLLLoss(reduction='none',weight=torch.Tensor([0.8,0.8,0.8,0.5]).cuda())
+        self.criterion = torch.nn.NLLLoss(reduction='none', weight=torch.Tensor([0.8, 0.8, 0.8, 0.5]).cuda())
 
     def forward(self, input, length, target):
         loss = []
@@ -38,106 +43,113 @@ class NLLSequenceLoss(torch.nn.Module):
         loss = (loss * mask).sum() / mask.sum()
         return loss
 
+
 def timedelta_string(timedelta):
     totalSeconds = int(timedelta.total_seconds())
-    hours, remainder = divmod(totalSeconds,60*60)
-    minutes, seconds = divmod(remainder,60)
+    hours, remainder = divmod(totalSeconds, 60 * 60)
+    minutes, seconds = divmod(remainder, 60)
     return "{:0>2} hrs, {:0>2} mins, {:0>2} secs".format(hours, minutes, seconds)
 
-def output_iteration(loss, i, time, totalitems):
 
-    avgBatchTime = time / (i+1)
+def output_iteration(loss, i, time, totalitems):
+    avgBatchTime = time / (i + 1)
     estTime = avgBatchTime * (totalitems - i)
-    
-    print("Iteration: {:0>8},Elapsed Time: {},Estimated Time Remaining: {},Loss:{}".format(i, timedelta_string(time), timedelta_string(estTime),loss))
+
+    print("Iteration: {:0>8},Elapsed Time: {},Estimated Time Remaining: {},Loss:{}".format(i, timedelta_string(time),
+                                                                                           timedelta_string(estTime),
+                                                                                           loss))
+
 
 class Trainer():
-
     tot_iter = 0
-    writer = SummaryWriter()    
-    
-    def __init__(self, options,model):
-        self.R='R'in options['general'].keys()
-        self.cls_num=options['general']['class_num']
-        self.use_plus=options['general']['use_plus']
+    writer = SummaryWriter()
+
+    def __init__(self, options, model):
+        self.R = 'R' in options['general'].keys()
+        self.cls_num = options['general']['class_num']
+        self.use_plus = options['general']['use_plus']
         self.use_slice = options['general']['use_slice']
         self.usecudnn = options["general"]["usecudnn"]
-        self.use_3d=options['general']['use_3d']
+        self.use_3d = options['general']['use_3d']
         self.batchsize = options["input"]["batchsize"]
-        self.use_lstm=options["general"]["use_lstm"]
+        self.use_lstm = options["general"]["use_lstm"]
         self.statsfrequency = options["training"]["statsfrequency"]
         self.learningrate = options["training"]["learningrate"]
         self.modelType = options["training"]["learningrate"]
         self.weightdecay = options["training"]["weightdecay"]
         self.momentum = options["training"]["momentum"]
         self.save_prefix = options["training"]["save_prefix"]
-        self.asinput=options['general']['plus_as_input']
-        self.USE_25D=options['general']['use25d']
+        self.asinput = options['general']['plus_as_input']
+        self.USE_25D = options['general']['use25d']
         if options['general']['use_slice']:
             if self.USE_25D:
                 f = 'data/3cls_train.list'
-                self.trainingdataset = NCPJPGtestDataset_new(options["training"]["data_root"],options["training"]["padding"],
-                                                f, cls_num=self.cls_num, mod=options['general']['mod'],options=options)
+                self.trainingdataset = NCPJPGtestDataset_new(options["training"]["data_root"],
+                                                             options["training"]["padding"],
+                                                             f, cls_num=self.cls_num, mod=options['general']['mod'],
+                                                             options=options)
             else:
                 self.trainingdataset = NCPJPGDataset_new(options["training"]["data_root"],
-                                                options["training"]["index_root"],
-                                                options["training"]["padding"],
-                                                True,cls_num=self.cls_num,mod=options['general']['mod'],options=options)
+                                                         options["training"]["index_root"],
+                                                         options["training"]["padding"],
+                                                         True, cls_num=self.cls_num, mod=options['general']['mod'],
+                                                         options=options)
         else:
             if options['general']['use_3d']:
                 self.trainingdataset = NCPDataset(
-                                                    options["training"]["index_root"],
-                                                    options["training"]["padding"],
-                                                    True,
-                                                    z_length=options["model"]["z_length"])
+                    options["training"]["index_root"],
+                    options["training"]["padding"],
+                    True,
+                    z_length=options["model"]["z_length"])
             else:
                 self.trainingdataset = NCP2DDataset(options["training"]["data_root"],
                                                     options["training"]["index_root"],
                                                     options["training"]["padding"],
-                                                    True)##TODO:3
+                                                    True)  ##TODO:3
         weights = self.trainingdataset.make_weights_for_balanced_classes()
         weights = torch.DoubleTensor(weights)
         sampler = torch.utils.data.sampler.WeightedRandomSampler(
             weights, len(self.trainingdataset))
 
         self.trainingdataloader = DataLoader(
-                                    self.trainingdataset,
-                                    batch_size=options["input"]["batchsize"],
-                                    #shuffle=options["input"]["shuffle"],
-                                    num_workers=options["input"]["numworkers"],
-                                    drop_last=True,
-                                    sampler=sampler
-                                    )
+            self.trainingdataset,
+            batch_size=options["input"]["batchsize"],
+            # shuffle=options["input"]["shuffle"],
+            num_workers=options["input"]["numworkers"],
+            drop_last=False,
+            sampler=sampler
+        )
 
-        self.optimizer = optim.Adam(model.parameters(),lr = self.learningrate,amsgrad=True)
-        #self.schedule=torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer,'max',
+        self.optimizer = optim.Adam(model.parameters(), lr=self.learningrate, amsgrad=True)
+        # self.schedule=torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer,'max',
         #                                                         patience=3, factor=.3, threshold=1e-3, verbose=True)
-        #optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+        # optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
         self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=1000, gamma=0.9)
-        self.model=model
+        self.model = model
         if self.use_3d:
-            self.criterion=self.model.loss()
+            self.criterion = self.model.loss()
         else:
-            #criterion=nn.
-            #w=torch.Tensor(self.trainingdataset.get_w()).cuda()
-            #print(w)
-            w = torch.Tensor([0.8,0.2]).cuda()
-            self.criterion =nn.NLLLoss(w).cuda()#0.3,0.7
+            # criterion=nn.
+            # w=torch.Tensor(self.trainingdataset.get_w()).cuda()
+            # print(w)
+            # w = torch.Tensor(w).cuda()
+            self.criterion = nn.NLLLoss().cuda()  # 0.3,0.7
             if self.use_plus:
                 self.criterion_age = nn.NLLLoss(ignore_index=-1).cuda()
                 self.criterion_gender = nn.NLLLoss(ignore_index=-1,
-                                              weight=torch.Tensor([0.3, 0.7]).cuda()).cuda()
-                self.criterion_pos=nn.SmoothL1Loss().cuda()
+                                                   weight=torch.Tensor([0.3, 0.7]).cuda()).cuda()
+                self.criterion_pos = nn.SmoothL1Loss().cuda()
         if self.use_lstm:
-            self.criterion=NLLSequenceLoss()
-        if(self.usecudnn):
+            self.criterion = NLLSequenceLoss()
+        if (self.usecudnn):
             self.net = nn.DataParallel(self.model).cuda()
             self.criterion = self.criterion.cuda()
 
-    def ScheduleLR(self,acc):
+    def ScheduleLR(self, acc):
         self.scheduler.step(acc)
-    def __call__(self,epoch):
-        #set up the loss function.
+
+    def __call__(self, epoch):
+        # set up the loss function.
         self.model.train()
         startTime = datetime.now()
         print("Starting training...")
@@ -147,31 +159,31 @@ class Trainer():
             labels = Variable(sample_batched['label'])
             length = len(sample_batched['length'][1])
             features = Variable(sample_batched['features'])
-            name=sample_batched['name']
+            name = sample_batched['name']
             if self.use_plus:
                 age = Variable(sample_batched['age']).cuda()
                 gender = Variable(sample_batched['gender']).cuda()
-                pos=Variable(sample_batched['pos']).cuda()
-           # break
+                pos = Variable(sample_batched['pos']).cuda()
+            # break
             if self.USE_25D:
                 input = input.squeeze(0)
                 input = input.permute(1, 0, 2, 3)
                 features = features.squeeze(0)
-            if(self.usecudnn):
+            if (self.usecudnn):
                 input = input.cuda()
                 labels = labels.cuda()
             if not self.use_plus:
                 if self.R:
-                    outputs,_ = self.net(input, features,True)
+                    outputs, _ = self.net(input, features, True)
                 else:
-                    outputs,_ = self.net(input,False)
+                    outputs, _ = self.net(input, False)
             else:
                 if self.asinput:
-                    outputs, _, _, _, deep_feaures = self.net(input,pos,gender,age)
+                    outputs, _, _, _, deep_feaures = self.net(input, pos, gender, age)
                 else:
-                    outputs,out_gender,out_age,out_pos,deep_feaures=self.net(input)
+                    outputs, out_gender, out_age, out_pos, deep_feaures = self.net(input)
             if self.use_3d or self.use_lstm:
-                loss = self.criterion(outputs, length,labels.squeeze(1))
+                loss = self.criterion(outputs, length, labels.squeeze(1))
             else:
                 loss = self.criterion(outputs, labels.squeeze(1))
             loss.backward()
@@ -179,13 +191,13 @@ class Trainer():
             self.scheduler.step()
             sampleNumber = i_batch * self.batchsize
 
-            if(sampleNumber % self.statsfrequency == 0):
+            if (sampleNumber % self.statsfrequency == 0):
                 currentTime = datetime.now()
-                output_iteration(loss.cpu().detach().numpy(), sampleNumber, currentTime - startTime, len(self.trainingdataset))
+                output_iteration(loss.cpu().detach().numpy(), sampleNumber, currentTime - startTime,
+                                 len(self.trainingdataset))
                 Trainer.writer.add_scalar('Train Loss', loss, Trainer.tot_iter)
             Trainer.tot_iter += 1
 
-
-        print("Epoch "+str(epoch)+"completed, saving state...")
+        print("Epoch " + str(epoch) + "completed, saving state...")
         print(self.use_3d)
         torch.save(self.model.state_dict(), "{}.pt".format(self.save_prefix))
--- validation.py
+++ validation.py
@@ -1,5 +1,6 @@
 from torch.autograd import Variable
 import warnings
+
 warnings.filterwarnings("ignore")
 import torch
 import torch.optim as optim
@@ -7,20 +8,20 @@ from datetime import datetime, timedelta
 
 from torch.utils.data import DataLoader
 import torch.nn as nn
-from data.dataset import NCPDataset,NCP2DDataset,NCPJPGDataset,NCPJPGDataset_new,NCPJPGtestDataset_new
-import os,cv2
+from data.dataset import NCPDataset, NCP2DDataset, NCPJPGDataset, NCPJPGDataset_new, NCPJPGtestDataset_new
+import os, cv2
 import numpy as np
 
-def _validate(modelOutput, length, labels, total=None, wrong=None):
 
+def _validate(modelOutput, length, labels, total=None, wrong=None):
     averageEnergies = torch.mean(modelOutput.data, 1)
     for i in range(modelOutput.size(0)):
-        #print(modelOutput[i,:length[i]].sum(0).shape)
-        averageEnergies[i] = modelOutput[i,:length[i]].mean(0)
+        # print(modelOutput[i,:length[i]].sum(0).shape)
+        averageEnergies[i] = modelOutput[i, :length[i]].mean(0)
 
     maxvalues, maxindices = torch.max(averageEnergies, 1)
-    #print(maxindices.cpu().numpy())
-    #print(labels.cpu().numpy())
+    # print(maxindices.cpu().numpy())
+    # print(labels.cpu().numpy())
     count = 0
 
     for i in range(0, labels.squeeze(1).size(0)):
@@ -34,120 +35,123 @@ def _validate(modelOutput, length, labels, total=None, wrong=None):
             count += 1
         else:
             if wrong is not None:
-               if l not in wrong:
-                   wrong[l] = 1
-               else:
-                   wrong[l] += 1
+                if l not in wrong:
+                    wrong[l] = 1
+                else:
+                    wrong[l] += 1
 
     return (averageEnergies, count)
 
+
 class Validator():
-    def __init__(self, options, mode,model,savenpy):
-        self.R='R' in options['general'].keys()
-        self.model=model
-        self.cls_num=options['general']['class_num']
+    def __init__(self, options, mode, model, savenpy):
+        self.R = 'R' in options['general'].keys()
+        self.model = model
+        self.cls_num = options['general']['class_num']
         self.use_plus = options['general']['use_plus']
-        self.use_3d=options['general']['use_3d']
+        self.use_3d = options['general']['use_3d']
         self.usecudnn = options["general"]["usecudnn"]
         self.use_lstm = options["general"]["use_lstm"]
         self.batchsize = options["input"]["batchsize"]
-        self.use_slice=options['general']['use_slice']
+        self.use_slice = options['general']['use_slice']
         self.asinput = options['general']['plus_as_input']
         self.USE_25D = options['general']['use25d']
         if options['general']['use_slice']:
             if self.USE_25D:
                 f = 'data/3cls_test.list'
-                self.validationdataset = NCPJPGtestDataset_new(options[mode]["data_root"],options["training"]["padding"],
-                                                             f, cls_num=self.cls_num, mod=options['general']['mod'],
+                self.validationdataset = NCPJPGtestDataset_new(options[mode]["data_root"],
+                                                               options["training"]["padding"],
+                                                               f, cls_num=self.cls_num, mod=options['general']['mod'],
                                                                options=options)
             else:
                 self.validationdataset = NCPJPGDataset_new(options[mode]["data_root"],
-                                                        options[mode]["index_root"],
-                                                        options[mode]["padding"],
-                                                        False,cls_num=self.cls_num,
-                                                        mod=options['general']['mod'],
+                                                           options[mode]["index_root"],
+                                                           options[mode]["padding"],
+                                                           False, cls_num=self.cls_num,
+                                                           mod=options['general']['mod'],
                                                            options=options)
         else:
             if options['general']['use_3d']:
-                self.validationdataset = NCPDataset(  options[mode]["index_root"],
-                                                      options[mode]["padding"],
-                                                      False,
+                self.validationdataset = NCPDataset(options[mode]["index_root"],
+                                                    options[mode]["padding"],
+                                                    False,
                                                     z_length=options["model"]["z_length"])
             else:
                 self.validationdataset = NCP2DDataset(options[mode]["data_root"],
-                                                        options[mode]["index_root"],
-                                                        options[mode]["padding"],
-                                                        False)
-        self.savingnpy=savenpy
+                                                      options[mode]["index_root"],
+                                                      options[mode]["padding"],
+                                                      False)
+        self.savingnpy = savenpy
 
         self.tot_data = len(self.validationdataset)
         self.validationdataloader = DataLoader(
-                                    self.validationdataset,
-                                    batch_size=options["input"]["batchsize"],
-                                    shuffle=True,
-                                    num_workers=options["input"]["numworkers"],
-                                    drop_last=False
-                                )
+            self.validationdataset,
+            batch_size=options["input"]["batchsize"],
+            shuffle=True,
+            num_workers=options["input"]["numworkers"],
+            drop_last=False
+        )
         self.mode = mode
-        self.epoch=0
-        
+        self.epoch = 0
+
     def __call__(self):
-        self.epoch+=1
+        self.epoch += 1
         with torch.no_grad():
             print("Starting {}...".format(self.mode))
-            count = np.zeros((self.cls_num+self.use_plus*2*(1-self.asinput)))
-            Matrix=np.zeros((self.cls_num,self.cls_num))
+            count = np.zeros((self.cls_num + self.use_plus * 2 * (1 - self.asinput)))
+            Matrix = np.zeros((self.cls_num, self.cls_num))
             if self.use_3d:
                 validator_function = self.model.validator_function()
             if self.use_lstm:
                 validator_function = _validate
                 self.model.eval()
-            LL=[]
-            GG=[]
-            AA=[]
-            if(self.usecudnn):
+            LL = []
+            GG = []
+            AA = []
+            if (self.usecudnn):
                 net = nn.DataParallel(self.model).cuda()
-            error_dir='error/'
-            os.makedirs(error_dir,exist_ok=True)
-            cnt=0
-            num_samples = np.zeros((self.cls_num+self.use_plus*2*(1-self.asinput)))
+            error_dir = 'error/'
+            os.makedirs(error_dir, exist_ok=True)
+            cnt = 0
+            num_samples = np.zeros((self.cls_num + self.use_plus * 2 * (1 - self.asinput)))
             for i_batch, sample_batched in enumerate(self.validationdataloader):
                 input = Variable(sample_batched['temporalvolume']).cuda()
                 labels = Variable(sample_batched['label']).cuda()
-                #length = len(sample_batched['length'][1])
-                names=sample_batched['name']
-                outputs,f = net(input)
+                # length = len(sample_batched['length'][1])
+                names = sample_batched['name']
+                outputs, f = net(input)
 
                 output_numpy = np.exp(outputs.cpu().numpy())
-                output_numpy=output_numpy[:,[0,-1]]
-                #output_numpy_ab=np.sum(output_numpy[:,1:],1)
-                #output_numpy=np.stack([output_numpy[:,0],output_numpy_ab],-1)
-                output_numpy=output_numpy/output_numpy.sum(1,keepdims=True)
-                pre=np.array(output_numpy[:,-1]>0.5,np.int)
-                isacc=labels.cpu().numpy().reshape(labels.size(0))==pre
-                label_numpy=labels.cpu().numpy()[:,0]
+                output_numpy = output_numpy[:, [0, -1]]
+                # output_numpy_ab=np.sum(output_numpy[:,1:],1)
+                # output_numpy=np.stack([output_numpy[:,0],output_numpy_ab],-1)
+                output_numpy = output_numpy / output_numpy.sum(1, keepdims=True)
+                pre = np.array(output_numpy[:, -1] > 0.5, np.int)
+                isacc = labels.cpu().numpy().reshape(labels.size(0)) == pre
+                label_numpy = labels.cpu().numpy()[:, 0]
 
-               # argmax = (-vector.cpu().numpy()).argsort()
+                # argmax = (-vector.cpu().numpy()).argsort()
                 for i in range(labels.size(0)):
-                    LL.append([names[i], output_numpy[i,-1], label_numpy[i]])
-                    Matrix[label_numpy[i],pre[i]]+=1
-                    if isacc[i]==1 :
+                    LL.append([names[i], output_numpy[i, -1], label_numpy[i]])
+                    Matrix[label_numpy[i], pre[i]] += 1
+                    if isacc[i] == 1:
                         count[labels[i]] += 1
-                    num_samples[labels[i]]+=1
-                    if self.mode=='validation':
-                        if labels[i] == 1 and output_numpy[i,-1]>0.99:
-                            I = np.array(input[i, :, :, :].cpu().numpy() * 255, np.uint8).transpose(1, 2, 0) [:,:, [2, 1, 0]]
+                    num_samples[labels[i]] += 1
+                    if self.mode == 'validation':
+                        if labels[i] == 1 and output_numpy[i, -1] > 0.99:
+                            I = np.array(input[i, :, :, :].cpu().numpy() * 255, np.uint8).transpose(1, 2, 0)[:, :,
+                                [2, 1, 0]]
                             cv2.imwrite('/mnt/data9/covid_detector_jpgs/selected_train1/abnor/abnor_' +
-                                        names[i].split('/')[-1].split('.')[0]+'.jpg',I)
-                        if labels[i] == 0 and output_numpy[i,0]>0.99:
-                            I = np.array(input[i, :, :, :].cpu().numpy() * 255, np.uint8).transpose(1, 2, 0) [:,:, [2, 1, 0]]
+                                        names[i].split('/')[-1].split('.')[0] + '.jpg', I)
+                        if labels[i] == 0 and output_numpy[i, 0] > 0.99:
+                            I = np.array(input[i, :, :, :].cpu().numpy() * 255, np.uint8).transpose(1, 2, 0)[:, :,
+                                [2, 1, 0]]
                             cv2.imwrite('/mnt/data9/covid_detector_jpgs/selected_train1/nor/nor_' +
-                                        names[i].split('/')[-1].split('.')[0]+'.jpg',I)
+                                        names[i].split('/')[-1].split('.')[0] + '.jpg', I)
 
-
-        print(count[:self.cls_num].sum()/num_samples[:self.cls_num].sum(),np.mean(AA))
-        LL=np.array(LL)
+        print(count[:self.cls_num].sum() / num_samples[:self.cls_num].sum(), np.mean(AA))
+        LL = np.array(LL)
         np.save(self.savingnpy, LL)
 
         print(Matrix)
-        return count/num_samples,count[:self.cls_num].sum()/num_samples[:self.cls_num].sum()
+        return count / num_samples, count[:self.cls_num].sum() / num_samples[:self.cls_num].sum()
